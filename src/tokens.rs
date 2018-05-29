944808fbd78f8c5df2bb4440a37584651068e27a
noxim <aaro.peramaa@gmail.com>
2018-05-29 21:18:11 +0300

    [tokenizer] Started adding token variants

--- src/tokens.rs
+++ src/tokens.rs
@@ -1,18 +1,36 @@
 use std::fmt::Debug;
 
+#[allow(unused)]
 #[derive(Debug)]
-pub struct Position {
-    pos: usize,
-    len: usize,
-}
+pub enum Token {
+    BraceOpen,  // {
+    BraceClose, // }
+    ParenOpen,  // (
+    ParenClose, // )
+    
+    Semi,    // ;
+    Plus,    // +
+    Minus,   // -
+    Star,    // *
+    Slash,   // /
+    Equal,   // =
+    Greater, // >
+    Less,    // <
+    Not,     // !
+    Period,  // .
 
-pub trait Token: Debug {
-    fn pos(&self) -> Position {
-        Position {
-            pos: 0,
-            len: 0,
-        }
-    }
+    KwStruct, // "struct"
+    KwEnum,   // "enum"
+    KwHas,    // "has"
+    KwIs,     // "is"
+    KwPure,   // "pure"
+    KwImpure, // "impure"
+    KwTask,   // "task"
+    KwIf,     // "if"
+    KwElse,   // "else"
+    KwElif,   // "elif"
+    KwMatch,  // "match"
+
 }
 
 pub fn tokenize(src: &str) -> Vec<Box<Token>> {
